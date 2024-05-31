db6ec8f9f8b5be77f973721bd19d85017049da18
Francisco Castillo <fr.castillou@duocuc.cl>
2024-05-31 12:01:25 -0400

    Agregado persona

--- greenhill/models.py
+++ greenhill/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    rut = models.CharField(max_length=13)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo = models.EmailField()
+    telefono = models.IntegerField(max_length=9)
+    region = models.IntegerChoices()
+    comuna = models.IntegerChoices()
+    direccion = models.CharField(max_length=100)
+    pass1 = models.CharField(max_length=16)
+    pass2 = models.CharField(max_length=16)
