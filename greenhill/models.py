9da3f22d1b30acc1a4ae7266a0d61ee4671a6f9e
Francisco Castillo <fr.castillou@duocuc.cl>
2024-06-07 11:55:56 -0400

    Agregados modelos

--- greenhill/models.py
+++ greenhill/models.py
@@ -7,9 +7,20 @@ class Persona(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     correo = models.EmailField()
-    telefono = models.IntegerField(max_length=9)
-    region = models.IntegerChoices()
-    comuna = models.IntegerChoices()
+    telefono = models.IntegerField()
     direccion = models.CharField(max_length=100)
     pass1 = models.CharField(max_length=16)
     pass2 = models.CharField(max_length=16)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    imagen = models.ImageField(upload_to='productos', null=True)
+
+class Pedido(models.Model):
+    fecha = models.DateField()
+    total = models.IntegerField()
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+
