246cd794d5adf1001244d07c3d651992e94238d4
Francisco Castillo <fr.castillou@duocuc.cl>
2024-06-07 11:55:14 -0400

    Modificado

--- greenhill/admin.py
+++ greenhill/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+class AdminPersona(admin.ModelAdmin):
+    list_display = []
+    search_fields = []
+    list_filter = []
+    list_per_page = 10
