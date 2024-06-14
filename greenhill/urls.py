73777498b889aa864957085ec8c7d7fd6ab592af
Francisco Castillo <fr.castillou@duocuc.cl>
2024-06-14 10:38:51 -0400

    Modificado

--- greenhill/urls.py
+++ greenhill/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',index,name='index'),
     path('admin-agregar',adminAgregar,name='admin-agregar'),
+    path('catalogo',catalogo,name='catalogo'),
+    path('producto/<id>',producto,name='producto'),
+    path('eliminar-producto/<int:id>/', eliminarProductos, name='eliminar-producto'),
+    path('editar-producto/<int:id>/', editarProductos, name='editar-producto'),
+    path('admin-productos', paginaProductos, name='admin-productos'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- greenhill/views.py
+++ greenhill/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render
 from .forms import * 
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -19,3 +20,45 @@ def adminAgregar(request):
         "form": form
     }
     return render(request, "greenhill/admin-agregar-producto.html", datos)
+
+def catalogo(request):
+    productos = Producto.objects.all()
+    datos = {
+        "productos": productos
+    }
+    return render(request, "greenhill/catalogo.html", datos)
+
+def producto(request,id):
+    producto = get_object_or_404(Producto, id=id)
+    datos = {
+        "producto": producto
+    }
+    return render(request,'greenhill/producto.html',datos)
+
+def paginaProductos(request):
+    productos = Producto.objects.all()
+    datos = {
+        "productos": productos
+    }
+    return render(request,'greenhill/admin-productos.html',datos)
+
+def editarProductos(request,id):
+    if request.method == 'POST':
+        producto = get_object_or_404(Producto, id=id)
+        producto.nombre = request.POST.get('nombre')
+        producto.precio = request.POST.get('precio')
+        producto.description = request.POST.get('descripcion')
+        producto.stock = request.POST.get('stock')
+        producto.imagen = request.FILES.get('imagen')
+        producto.save()
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+
+def eliminarProductos(request, id):
+    if request.method == 'POST':
+        producto = get_object_or_404(Producto, id=id)
+        producto.delete()
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
